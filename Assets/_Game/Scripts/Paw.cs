924167789f1b1b24840989bf0c1a6bd99c644695
TheKleee <mk.kleee@gmail.com>
2022-02-23 09:34:54 +0100

    paws xD

--- Assets/_Game/Scripts/Paw.cs
+++ Assets/_Game/Scripts/Paw.cs
@@ -21,10 +21,22 @@ public class Paw : MonoBehaviour
             gameObject.SetActive(false);
     }
 
-
+    List<GameObject> fingers = new List<GameObject>(); 
     public void ShowPaw()
     {
         if (lvlComplete)
+        {
             gameObject.SetActive(true);
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (Physics.Raycast(transform.GetChild(i).position, -Vector3.up, out RaycastHit hit, 25.0f))
+                    if (hit.transform.CompareTag("Fish"))
+                        fingers.Add(transform.GetChild(i).gameObject);
+
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+            for (int i = 0; i < fingers.Count; i++)
+                fingers[i].SetActive(true);
+        }
     }
 }
